0b4321c2a9bc3813b91f1ad0e1dd7d5adaa24d5a
Fading-L <413347132l@GMAIL.COM>
2025-06-17 00:29:50 +0800

    重构代码

---
+++ config/routes.go
@@ -0,0 +1,13 @@
+ package config
+
+ import (
+  "github.com/gin-gonic/gin"
+  "github.com/Fading-L/gin-api/api"
+)
+func Routes(r *gin.Engine)  {
+  
+  // 这里可以添加路由规则
+  //  r := gin.Default()
+ r.GET("/ping",  api.Ping )
+ 
+}
--- main.go
+++ main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-  "net/http"
-
-  "github.com/gin-gonic/gin"
+ "github.com/gin-gonic/gin"
+ "github.com/Fading-L/gin-api/config"
 )
 
 func main() {
   r := gin.Default()
-  r.GET("/ping", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{
-      "message": "pong2",
-    })
-  })
+	config.Routes(r)
   r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
